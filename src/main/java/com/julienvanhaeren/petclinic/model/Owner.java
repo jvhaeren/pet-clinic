a072e0d44c1e055007029b6e2953ee586dc43ade
jvhaeren <julienvanhaeren@gmail.com>
2019-10-07 09:41:59 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/julienvanhaeren/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.julienvanhaeren.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/julienvanhaeren/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.julienvanhaeren.petclinic.model;
+
+public class Vet extends Person {
+}
