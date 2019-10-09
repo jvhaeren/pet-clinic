138f02c01771fd942c577459104f62e252725248
jvhaeren <julienvanhaeren@gmail.com>
2019-10-09 14:48:30 +0200

    Updated setting of the ID's. closes #25.

--- pet-clinic-data/src/main/java/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/petclinic/services/map/VetServiceMap.java
@@ -20,9 +20,7 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
     }
 
     @Override
-    public Vet save(Vet object) {
-        return super.save(object.getId(), object);
-    }
+    public Vet save(Vet object) { return super.save(object); }
 
     @Override
     public void delete(Vet object) {
