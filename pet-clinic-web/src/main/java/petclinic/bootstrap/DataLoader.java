88780e7cadf4945e65578c97d005adeb9fbba713
jvhaeren <julienvanhaeren@gmail.com>
2019-10-22 14:20:30 +0200

    Created Visit Map Service and Visit Spring Data JPA Service. Visit on startup in bootstrap. Closes #48, Closes #49, Closes #50

--- pet-clinic-web/src/main/java/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/petclinic/bootstrap/DataLoader.java
@@ -3,10 +3,7 @@ package petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import petclinic.model.*;
-import petclinic.services.OwnerService;
-import petclinic.services.PetTypeService;
-import petclinic.services.SpecialityService;
-import petclinic.services.VetService;
+import petclinic.services.*;
 
 import java.time.LocalDate;
 
@@ -17,14 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
@@ -89,6 +87,12 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(patsCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(patsCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("sneezing kitty");
+        visitService.save(catVisit);
 
         System.out.println("Loaded owners....");
 
